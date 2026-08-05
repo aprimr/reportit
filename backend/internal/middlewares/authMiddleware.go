b4d70fdf536e693539a9cfb7a4b88823be732675
Aprim <aprimregmi24@gmail.com>
2026-08-05 12:31:53 +0545

    feat(auth): add auth middleware

---
+++ backend/internal/middlewares/authMiddleware.go
@@ -0,0 +1,74 @@
+package middlewares
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/aprimr/reportit/internal/utils"
+)
+
+type contextKey string
+
+const (
+	UserUidKey         contextKey = "uid"
+	UserEmailKey       contextKey = "email"
+	UserPhoneKey       contextKey = "phone"
+	UserAccessTokenKey contextKey = "access_token"
+)
+
+func AuthMiddleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// Extract Auth header from request
+			authHeader := r.Header.Get("Authorization")
+			if authHeader == "" {
+				utils.WriteError(w, http.StatusUnauthorized, "auth header is missing")
+				return
+			}
+
+			// Extract access token from auth header
+			parts := strings.Split(authHeader, " ")
+			if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
+				http.Error(w, "invalid authorization header format", http.StatusUnauthorized)
+				return
+			}
+			tokenString := parts[1]
+
+			// Verify  JWT token
+			claims, err := utils.ValidateToken(tokenString, false)
+			if err != nil {
+				http.Error(w, "invalid or expired access token", http.StatusUnauthorized)
+				return
+			}
+
+			ctx := context.WithValue(r.Context(), UserUidKey, claims["uid"])
+			ctx = context.WithValue(ctx, UserEmailKey, claims["email"])
+			ctx = context.WithValue(ctx, UserPhoneKey, claims["phone"])
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
+
+// Helper functions
+func GetUidFromContext(ctx context.Context) string {
+	if uid, ok := ctx.Value(UserUidKey).(string); ok {
+		return uid
+	}
+	return ""
+}
+
+func GetEmailFromContext(ctx context.Context) string {
+	if email, ok := ctx.Value(UserEmailKey).(string); ok {
+		return email
+	}
+	return ""
+}
+
+func GetPhoneFromContext(ctx context.Context) string {
+	if phone, ok := ctx.Value(UserPhoneKey).(string); ok {
+		return phone
+	}
+	return ""
+}
